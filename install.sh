a8024b5d07cca62d6b6475b1e2a99ede0b429398
Ubuntu <ubuntu@ip-172-31-36-126.us-west-2.compute.internal>
2018-02-20 01:56:03 +0000

    fixed collors

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-cp -r colors ~/.vim/ 
+cp -r colors ~/.vim/colors 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
-cp .tmux.conf ~/.tmux.conf
 vim +PluginInstall +qall
